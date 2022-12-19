e2246e7ba537c17976464ebc83e5d173c95a6c46
Maromshi <58001533+Maromshi@users.noreply.github.com>
2022-12-19 14:55:28 +0200

    change log Expressions

--- Candidate/script.js
+++ Candidate/script.js
@@ -1,23 +1,50 @@
 
 let personDetails = {
     name: "",
-    lastName:"",
-    email:"",
-    Id:"",
-    job:""
+    lastName: "",
+    email: "",
+    Id: "",
+    job: ""
 }
-const convertToJSON =(object) => {
+const convertToJSON = (object) => {
     let JSONPerson = JSON.stringify(object)
     console.log(JSONPerson)
 
 }
+const checkDetails = () => {
+
+}
 const sendDetails = () => {
-    personDetails.name= document.getElementById("firstName").value    
-    personDetails.lastName= document.getElementById("lastName").value
-    personDetails.email= document.getElementById("email").value
-    personDetails.Id= document.getElementById("personId").value
-    personDetails.job = document.getElementById("jobList").value
-    console.log(personDetails)
-    convertToJSON(personDetails)
+    const name = document.getElementById("firstName").value
+    const lastName = document.getElementById("lastName").value
+    const email = document.getElementById("email").value
+    const Id = document.getElementById("personId").value
+    const job = document.getElementById("jobList").value
+
+    if (/[0-9]/.test(name) || /[0-9]/.test(lastName)) {
+        alert("Error input")
+    }
+    else {
+
+        personDetails.name = name
+        personDetails.lastName = lastName
+        personDetails.email = email
+        personDetails.Id = Id
+        personDetails.job = job
+        console.log(personDetails)
+        convertToJSON(personDetails)
+        checkDetails()
+    }
+
 }
 
+const enableButton = () => {
+    //console.log("clicked")
+    let check = document.getElementById("checkBox")
+    let btn  = document.getElementById("button")
+    if(check.checked)
+    {
+     btn.disabled= false
+    }
+    
+}
